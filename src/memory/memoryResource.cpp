5cbe533a3323f2c7dd02dd4c79e12ad5251c37bf
AomaYple <2890599214@qq.com>
2025-01-31 16:52:40 +0800

    移除getSyncMemoryResource

--- src/memory/memoryResource.cpp
+++ src/memory/memoryResource.cpp
@@ -2,27 +2,14 @@
 
 #ifdef NDEBUG
     #include "MiMallocResource.hpp"
-
-namespace {
-    constinit coContext::internal::MiMallocResource miMallocResource;
-}    // namespace
 #endif    // NDEBUG
 
-auto coContext::internal::getUnSyncMemoryResource() -> std::pmr::memory_resource * {
+auto coContext::internal::getMemoryResource() -> std::pmr::memory_resource * {
 #ifndef NDEBUG
     thread_local std::pmr::unsynchronized_pool_resource resource;
 #else     // NDEBUG
+    static MiMallocResource miMallocResource;
     thread_local std::pmr::unsynchronized_pool_resource resource{std::addressof(miMallocResource)};
-#endif    // NDEBUG
-
-    return std::addressof(resource);
-}
-
-auto coContext::internal::getSyncMemoryResource() -> std::pmr::memory_resource * {
-#ifndef NDEBUG
-    static std::pmr::synchronized_pool_resource resource;
-#else     // NDEBUG
-    static std::pmr::synchronized_pool_resource resource{std::addressof(miMallocResource)};
 #endif    // NDEBUG
 
     return std::addressof(resource);
--- src/memory/memoryResource.hpp
+++ src/memory/memoryResource.hpp
@@ -3,7 +3,5 @@
 #include <memory_resource>
 
 namespace coContext::internal {
-    [[nodiscard]] auto getUnSyncMemoryResource() -> std::pmr::memory_resource *;
-
-    [[nodiscard]] auto getSyncMemoryResource() -> std::pmr::memory_resource *;
+    [[nodiscard]] auto getMemoryResource() -> std::pmr::memory_resource *;
 }    // namespace coContext::internal
